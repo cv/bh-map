4cb1092ecfcafd5c509bf957c7ed353dc1465f95
Bruno Trecenti <btrecent@thoughtworks.com>
2015-05-29 11:26:04 -0300

    initial commit

---
+++ js/data.js
@@ -0,0 +1,25 @@
+var bh = bh || {};
+
+bh.rooms = [
+  { id: 'grace-hopper', name: 'Grace Hopper' },
+  { id: 'ada-lovelace', name: 'Ada Lovelace' },
+  { id: 'alan-turing', name: 'Alan Turing' },
+  { id: 'aaron-swartz', name: 'Aaron Swartz' },
+  { id: 'malala-yousafzai', name: 'Malala Yousafzai' },
+  { id: 'dandara', name: 'Dandara' },
+  { id: 'maria-da-penha', name: 'Maria da Penha' },
+  { id: 'chica-da-silva', name: 'Chica da Silva' },
+  { id: 'luca-bastos', name: 'Luca Bastos' },
+  { id: 'inconfidentes', name: 'Inconfidentes' },
+  { id: 'velho-chico', name: 'Velho Chico' },
+  { id: 'aleijadinho', name: 'Aleijadinho' }
+];
+
+bh.areas = [
+  { id: 'entrance', name: 'Entrance' },
+  { id: 'project-area', name: 'Project Area' },
+  { id: 'ops', name: 'Operations' },
+  { id: 'wc-ffa', name: 'WC Free For All' },
+  { id: 'wc', name: 'WC' },
+  { id: 'lounge', name: 'Lounge' },
+];
