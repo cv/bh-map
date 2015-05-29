99a60f612ee5d5c0dfaa7a762dd6c24d50d50eb6
Bruno Trecenti <btrecent@thoughtworks.com>
2015-05-29 17:12:04 -0300

    Updates

--- publish.js
+++ publish.js
@@ -2,5 +2,9 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(__dirname, function(err) {
-  if (err) { console.log('Publishing failed' + err); }
+  if (err) {
+    console.log('Publishing failed' + err);
+  } else {
+    console.log('Published to GitHub pages');
+  }
 });
