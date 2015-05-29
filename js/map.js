dde81e7d94c93ef3ad84def5335424f03d3ab708
Bruno Trecenti <btrecent@thoughtworks.com>
2015-05-29 18:27:43 -0300

    Updates

--- js/map.js
+++ js/map.js
@@ -1,47 +1,101 @@
 var bh = bh || {};
+function addHighlighListeners(link, svg) {
+  link.addEventListener('mouseover', function () {
+    svg.classList.add('highlight');
+  });
+
+  link.addEventListener('mouseleave', function () {
+    svg.classList.remove('highlight');
+  });
+
+  svg.addEventListener('mouseover', function () {
+    link.classList.add('highlight');
+    svg.classList.add('highlight');
+  });
+
+  svg.addEventListener('mouseleave', function () {
+    link.classList.remove('highlight');
+    svg.classList.remove('highlight');
+  });
+}
+
+function populateList(listId, items) {
+  var ul = document.getElementById(listId);
+
+  items.forEach(function (item) {
+    var li, a, svg;
+
+    li = document.createElement('li');
+    a = document.createElement('a');
+    svg = document.getElementById(item.id);
+
+    a.href = '#' + item.id;
+    a.text = item.name;
+    li.appendChild(a);
+    ul.appendChild(li);
+    svg.classList.add(listId);
+
+    addHighlighListeners(a, svg);
+  });
+}
+
+function clearPreviousView() {
+  var roomDetails = document.getElementById('room-details');
+  while (roomDetails.firstChild) {
+    roomDetails.removeChild(roomDetails.firstChild);
+  }
+}
+
+function populateRoomDetailData(view, data) {
+  var specs;
+  view.querySelector('h1').innerHTML = data.name;
+  view.querySelector('section[role=description] p').innerHTML = data.description;
+  specs = view.querySelector('section[role=specs]');
+
+  data.specs.forEach(function (spec) {
+    var li = document.createElement('li');
+    li.innerHTML = spec;
+    specs.querySelector('ul').appendChild(li);
+  });
+}
+
+function addNewView(hash) {
+  var view, template, roomDetails, data;
+  function byId(room) { return room.id === hash; }
+
+  roomDetails = document.getElementById('room-details');
+  template = document.getElementById('room-details-template');
+  view = document.importNode(template.content, true);
+  data = bh.rooms.filter(byId)[0];
+
+  if (data) {
+    populateRoomDetailData(view, data);
+    roomDetails.appendChild(view);
+  }
+}
+
+function changeView(hash) {
+  clearPreviousView();
+  if (hash) {
+    addNewView(hash);
+    Array.prototype.forEach.call(document.querySelectorAll('.highlight'), function (element) {
+      element.classList.remove('highlight');
+    });
+    document.querySelector('section[role=lists] a[href="#'+hash +'"]').classList.add('highlight');
+  }
+}
+
+function listenToHashChange() {
+  window.addEventListener('hashchange', function (e) {
+    var hash = e.newURL.split('#')[1];
+    changeView(hash);
+  });
+}
+
 
 window.addEventListener('load', function () {
-
-  function addHighlighListeners(link, svg) {
-    link.addEventListener('mouseover', function () {
-      svg.classList.add('highlight');
-    });
-
-    link.addEventListener('mouseleave', function () {
-      svg.classList.remove('highlight');
-    });
-
-    svg.addEventListener('mouseover', function () {
-      link.classList.add('highlight');
-      svg.classList.add('highlight');
-    });
-
-    svg.addEventListener('mouseleave', function () {
-      link.classList.remove('highlight');
-      svg.classList.remove('highlight');
-    });
-  }
-
-  function populateList(listId, items) {
-    var ul = document.getElementById(listId);
-
-    items.forEach(function (item) {
-      var li, a, svg;
-
-      li = document.createElement('li');
-      a = document.createElement('a');
-      svg = document.getElementById(item.id);
-
-      a.href = '#' + item.id;
-      a.text = item.name;
-      li.appendChild(a);
-      ul.appendChild(li);
-      svg.classList.add(listId);
-
-      addHighlighListeners(a, svg);
-    });
-  }
-
   populateList('rooms', bh.rooms);
   populateList('areas', bh.areas);
+  listenToHashChange();
+  changeView(window.location.hash.slice(1));
 });
